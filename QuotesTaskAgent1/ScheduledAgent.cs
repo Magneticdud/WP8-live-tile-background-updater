597a440de56d0ead10cc1f265c93c037645fe9e6
Magnetic_dud <mail@dandandin.it>
2014-07-17 19:24:35 +0200

    Didn't ignore what I had to ignore

---
+++ QuotesTaskAgent1/ScheduledAgent.cs
@@ -0,0 +1,74 @@
+﻿using System.Diagnostics;
+using System.Windows;
+using Microsoft.Phone.Scheduler;
+using Quotes.DataAccess;
+using Microsoft.Phone.Shell;
+using System.Linq;
+using System;
+
+namespace QuotesTaskAgent1
+{
+    public class ScheduledAgent : ScheduledTaskAgent
+    {
+        /// <remarks>
+        /// ScheduledAgent constructor, initializes the UnhandledException handler
+        /// </remarks>
+        static ScheduledAgent()
+        {
+            // Subscribe to the managed exception handler
+            Deployment.Current.Dispatcher.BeginInvoke(delegate
+            {
+                Application.Current.UnhandledException += UnhandledException;
+            });
+        }
+
+        /// Code to execute on Unhandled Exceptions
+        private static void UnhandledException(object sender, ApplicationUnhandledExceptionEventArgs e)
+        {
+            if (Debugger.IsAttached)
+            {
+                // An unhandled exception has occurred; break into the debugger
+                Debugger.Break();
+            }
+        }
+
+        /// <summary>
+        /// Agent that runs a scheduled task
+        /// </summary>
+        /// <param name="task">
+        /// The invoked task
+        /// </param>
+        /// <remarks>
+        /// This method is called when a periodic or resource intensive task is invoked
+        /// </remarks>
+        protected override void OnInvoke(ScheduledTask task)
+        {
+            //TODO: Add code to perform your task in background
+            if (task is PeriodicTask)
+            {
+                QuotesRepository repository = new QuotesRepository();
+                string quotes = repository.GetRandomQuote();
+                
+                FlipTileData flipTileData = new FlipTileData()
+                {
+                    BackContent = quotes,
+                    WideBackContent = quotes
+
+                };
+
+                ShellTile appTile = ShellTile.ActiveTiles.First();
+                if (appTile != null)
+                {
+                    appTile.Update(flipTileData);
+                }
+            }
+
+            #if DEBUG
+              ScheduledActionService.LaunchForTest(task.Name, TimeSpan.FromSeconds(60));
+            #endif
+
+
+            NotifyComplete();
+        }
+    }
+}
---
+++ WPBackgroundAgentApp/MainPage.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using WPBackgroundAgentApp.Resources;
+using Microsoft.Phone.Scheduler;
+using Quotes.DataAccess;
+
+namespace WPBackgroundAgentApp
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+            RegisterAgent();
+            // Sample code to localize the ApplicationBar
+            //BuildLocalizedApplicationBar();
+        }
+
+        private void RegisterAgent()
+        {
+            string taskName = "RandomQuoteTask";
+            try
+            {
+
+                if (ScheduledActionService.Find(taskName) != null)
+                {
+                    //if the agent exists, remove and then add it to ensure
+                    //the agent's schedule is updated to avoid expiration
+                    ScheduledActionService.Remove(taskName);
+                }
+
+                PeriodicTask periodicTask = new PeriodicTask(taskName);
+                periodicTask.Description = "Random Quote Update On Tile";
+                ScheduledActionService.Add(periodicTask);
+
+#if DEBUG
+                ScheduledActionService.LaunchForTest(taskName, TimeSpan.FromSeconds(30));
+#endif
+            }
+            catch (InvalidOperationException exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+            catch (SchedulerServiceException schedulerException)
+            {
+                MessageBox.Show(schedulerException.Message);
+            }
+        }
+
+        private void Button_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            QuotesRepository repository = new QuotesRepository();
+            txtQuote.Text = repository.GetRandomQuote();
+        }
+
+        // Sample code for building a localized ApplicationBar
+        //private void BuildLocalizedApplicationBar()
+        //{
+        //    // Set the page's ApplicationBar to a new instance of ApplicationBar.
+        //    ApplicationBar = new ApplicationBar();
+
+        //    // Create a new button and set the text value to the localized string from AppResources.
+        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
+        //    appBarButton.Text = AppResources.AppBarButtonText;
+        //    ApplicationBar.Buttons.Add(appBarButton);
+
+        //    // Create a new menu item with the localized string from AppResources.
+        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
+        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
+        //}
+    }
+}
